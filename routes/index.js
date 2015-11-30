db878d2149f10b99c57eb92912e789e35198ca09
robermorji <robermorji@gmail.com>
2015-11-30 11:53:06 +0100

    Creacion de REadme

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 var express=require('express');
 var app = express();
 var path = require('path');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 var mongo = require("mongodb").MongoClient;
 
 var empresas = new Array();
